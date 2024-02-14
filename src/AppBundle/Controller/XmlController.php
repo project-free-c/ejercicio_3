3e452f9b19d8f4689eaeef500b0aee3cc334848d
niba <nibachavez291@gmail.com>
2024-02-14 09:57:53 -0300

    V1.0.7

--- src/AppBundle/Controller/XmlController.php
+++ src/AppBundle/Controller/XmlController.php
@@ -15,19 +15,11 @@ class XmlController extends Controller
     public function parseXmlAction()
     {   
 
-        $xmlModel = new xmlModel();
+        // $xmlModel = new xmlModel();
+        // $data     = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_1error.xml");     
+        // $data2    = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_2errores.xml");  
 
-        $data     = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_1error.xml");     
-        $data2     = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_2errores.xml");  
-        print_r($data);
-        print_r($data2); 
-        die;    
-           
-        return $this->render('your_template.html.twig', [
-        
-        'data'   => $data,
-        
-        ]);
+        return $this->render("AppBundle::test.html.twig");
     
     }
 
