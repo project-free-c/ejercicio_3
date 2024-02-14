87e8cb8f947d303e4845d70e365850177800c553
Estebancardenas27 <estebancardenas2796@gmail.com>
2024-02-14 17:06:53 -0300

    addFunctionFileName

--- src/AppBundle/Controller/XmlController.php
+++ src/AppBundle/Controller/XmlController.php
@@ -17,12 +17,11 @@ class XmlController extends Controller
 
         $xmlModel = new xmlModel();
 
-        $data     = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_1error.xml");     
+        $data      = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_1error.xml");     
         $data2     = $xmlModel->ReadXmlFile("/code/ArchivosXML/Rechazo_2errores.xml");  
         print_r($data);
         print_r($data2); 
-        die;    
-           
+        die;               
         return $this->render('your_template.html.twig', [
         
         'data'   => $data,
